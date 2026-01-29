1a9290225ead51e8099d1281907a6e986f6da054
igunereve <igunereve@gmail.com>
2026-01-29 21:49:24 +0200

    updaes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>Design and coded by Catalin Pirvulescu. All rights reserved!</p>
+      <p>Designed and coded by Catalin Pirvulescu. All rights reserved!</p>
     </footer>
   );
 }
